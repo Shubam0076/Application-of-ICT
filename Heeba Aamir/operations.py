dcd4f79657af1b0a0b5a6e82822d917018d7e344
Heeba <heeba.aamir@gmail.com>
2025-09-22 20:04:03 +0500

    Added Heeba's folder with tasks

---
+++ Heeba Aamir/operations.py
@@ -0,0 +1,19 @@
+#arithmetic operations
+a = 23
+b = 46
+
+print("Addition;", a+b)
+print("Subtracion", a-b)
+print("Multiplication", a*b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
+
+#RELATIONAL OPERATIONS
+x = a!=10
+y = b==46
+z = a<23
+print(x,y,z)
+
+#Logical Operations
---
+++ Heeba Aamir/print.py
@@ -0,0 +1,19 @@
+name = "heeba" 
+age = 19
+height = 5.7
+is_student = True #FALSE
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+name = input ("Enter your name:")
+age = int(input("Enter your age:"))
+height = float(input("Enter your height:"))
+is_student = bool(input("Enter your is_student:"))
+
+print(name)
+print(age)
+print(height)
+print(is_student)
