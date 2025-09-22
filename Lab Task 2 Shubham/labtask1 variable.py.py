ee5481eade8d4990ad1577d86d2821baf24afcd8
Shubham <shubamsachdev@gmail.com>
2025-09-22 09:59:20 -0700

    Added Shubham's folder with task

---
+++ Lab Task 2 Shubham/labtask1 variable.py.py
@@ -0,0 +1,9 @@
+print("hello world")
+Name="Shubam"
+Age=17
+Height=5.3
+is_student=True
+print("Name:", Name)
+print("Age:", Age)
+print("Height", Height)
+print("Student", is_student)
---
+++ Lab Task 2 Shubham/labtask2 arthmetic.py.py
@@ -0,0 +1,9 @@
+a =8
+b=9
+print("Addition:",a+b)
+print("Subtractiom:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Floor Division:", a//b)
+print("Modulus",a%b)
+print("Exponent power", a**b)
